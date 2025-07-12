06b64aac28999cd7d55cbbbb344e516d6f8bd615
Nikita Bilous <nikita@bilous.me>
2025-07-12 22:32:19 +0400

    force all tests run on same event loop

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from typing import AsyncGenerator
 import pytest
 from httpx import ASGITransport
 from httpx import AsyncClient
+from pytest_asyncio import is_async_test
 from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import async_sessionmaker
@@ -33,15 +34,13 @@ async def setup_db():
 
 @pytest.fixture(scope='session')
 async def engine():
-    # FAFO with isolation_level
-    engine = create_async_engine(settings.DATABASE_URL, echo=True, isolation_level='AUTOCOMMIT')
+    engine = create_async_engine(settings.DATABASE_URL, echo=True)
     yield engine
     await engine.dispose()
 
 
 @pytest.fixture(scope='session')
 async def db(engine: AsyncEngine) -> AsyncGenerator[AsyncSession]:
-    engine.execution_options(isolation_level='READ COMMITTED')
     sess = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     async with sess() as s:
         async with sess.begin():
@@ -64,3 +63,13 @@ async def client(app):
 @pytest.fixture()
 async def user_repo(db) -> UserRepository:
     return UserRepository(db)
+
+
+def pytest_collection_modifyitems(items):
+    """
+    force all test to run in same event loop
+    """
+    pytest_asyncio_tests = (item for item in items if is_async_test(item))
+    session_scope_marker = pytest.mark.asyncio(loop_scope="session")
+    for async_test in pytest_asyncio_tests:
+        async_test.add_marker(session_scope_marker, append=False)
